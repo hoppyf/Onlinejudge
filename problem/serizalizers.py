f1491ca90bbac527fd7141cb610901ac0835d325
hoppy <554248181@qq.com>
2017-04-07 18:01:36 +0800

    fix

--- problem/serizalizers.py
+++ problem/serizalizers.py
@@ -33,7 +33,7 @@ class CreateProblemSerializer(serializers.Serializer):
     difficulty = serializers.IntegerField()
     tags = serializers.ListField(child=serializers.CharField(max_length=30))
     hint = serializers.CharField(allow_blank=True)
-    source = serializers.CharField(max_length=10, required=False)
+    source = serializers.CharField(required=False, max_length=10)
     visible = serializers.BooleanField()
 
 
